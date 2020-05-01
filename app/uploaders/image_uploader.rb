4ec5ac1f86eb874694287fcb8fb91d6fffabfd64
yujiasano <forever2011.0903@gmail.com>
2020-05-01 11:43:52 +0900

    s3 setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,8 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
   
   process resize_to_fit: [200, 200]
   # Override the directory where uploaded files will be stored.
